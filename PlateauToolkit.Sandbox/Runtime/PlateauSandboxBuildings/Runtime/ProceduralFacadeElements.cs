f98ac8661124d8f3343cbf5e9af2ed217005f161
Sacchy <mandw.entertainment@gmail.com>
2024-09-23 19:21:17 +0900

    マンションの窓の形状の修正

--- PlateauToolkit.Sandbox/Runtime/PlateauSandboxBuildings/Runtime/ProceduralFacadeElements.cs
+++ PlateauToolkit.Sandbox/Runtime/PlateauSandboxBuildings/Runtime/ProceduralFacadeElements.cs
@@ -53,7 +53,7 @@ namespace PlateauToolkit.Sandbox.Runtime.PlateauSandboxBuildings.Runtime
 
         protected const float k_WindowDepth = 0.1f;
         protected const float k_WindowWidthOffset = 0.5f;
-        protected const float k_WindowBottomOffset = 1;
+        protected const float k_WindowBottomOffset = 0; // 値を変えると窓の高さが変わる
         protected const float k_WindowTopOffset = 0.3f;
         protected const float k_WindowFrameWidth = 0.05f;
         protected const float k_WindowFrameRodWidth = 0.05f;
